69d44f34926efd9bc9d0152d83f82d23deb57a73
acatiadroid <cooldudequzons@gmail.com>
2022-12-02 12:34:13 +0000

    day 1 complete

--- 1/day1.py
+++ 1/day1.py
@@ -17,8 +17,11 @@ for item in range(0, len(calories) - 1):
 # Get the elf with the most calories
 most_cals = max(total_elves.values())
 
-print(total_elves)
+print(f"most calories is {most_cals}")
 
-for key, value in total_elves.items():
-    if most_cals == value:
-        print(f"elf with most calories is elf #{key}")
+# PART 2
+
+top3 = sorted(total_elves.values(), reverse=True)
+total = top3[0] + top3[1] + top3[2]
+
+print(f"total calories of top 3 elfs is {total}")
